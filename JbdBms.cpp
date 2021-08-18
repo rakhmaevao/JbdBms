09e7486b0775ce017774c93987e5379fde5576df
Рахмаев Александр <rahmaevao@gmail.com>
2021-08-18 17:06:47 +0300

    Auto-formatting

--- JbdBms.cpp
+++ JbdBms.cpp
@@ -7,40 +7,45 @@
 
 #include "JbdBms.h"
 
-
-JbdBms::JbdBms(HardwareSerial * t_hardwareSerial){
+JbdBms::JbdBms(HardwareSerial *t_hardwareSerial)
+{
   m_port = t_hardwareSerial;
   m_hwserial = true;
-  static_cast<HardwareSerial*>(m_port)->begin(9600);
+  static_cast<HardwareSerial *>(m_port)->begin(9600);
 }
 
-
-JbdBms::JbdBms(SoftwareSerial * t_softwareSerial){
+JbdBms::JbdBms(SoftwareSerial *t_softwareSerial)
+{
   m_port = t_softwareSerial;
   m_hwserial = false;
-  static_cast<SoftwareSerial*>(m_port)->begin(9600);
+  static_cast<SoftwareSerial *>(m_port)->begin(9600);
 }
 
-JbdBms::JbdBms(int rx, int tx){
+JbdBms::JbdBms(int rx, int tx)
+{
   SoftwareSerial mySerial(rx, tx);
   m_port = &mySerial;
   m_hwserial = false;
-  static_cast<SoftwareSerial*>(m_port)->begin(9600);
+  static_cast<SoftwareSerial *>(m_port)->begin(9600);
 }
 
 /*!
  * \brief Reading basic data from bms
  * \retval status of reading
  */
-bool JbdBms::readBmsData(){
+bool JbdBms::readBmsData()
+{
   uint8_t responce[BMS_LEN_RESPONCE];
 
   sendReqBasicMessage();
   readResponce(responce);
 
-  if (checkCheckSumRecieve(responce) == true) {
+  if (checkCheckSumRecieve(responce) == true)
+  {
     parseReqBasicMessage(responce);
-  } else {
+  }
+  else
+  {
     return false;
   }
   return true;
@@ -49,25 +54,31 @@ bool JbdBms::readBmsData(){
  * \brief Reading pack data from bms
  * \retval status of reading
  */
-bool JbdBms::readPackData(){
+bool JbdBms::readPackData()
+{
   uint8_t responce[BMS_LEN_RESPONCE];
 
   sendCellMessage();
   readResponce(responce);
 
-  if (checkCheckSumRecieve(responce) == true) {
+  if (checkCheckSumRecieve(responce) == true)
+  {
     parseReqPackMessage(responce);
-  } else {
+  }
+  else
+  {
     return false;
   }
   return true;
 }
 
-float JbdBms::getVoltage() {
+float JbdBms::getVoltage()
+{
   return m_voltage;
 }
 
-float JbdBms::getCurrent() {
+float JbdBms::getCurrent()
+{
   return m_current;
 }
 
@@ -80,56 +91,62 @@ uint16_t JbdBms::getProtectionState()
   return m_protectionState;
 }
 
-uint16_t JbdBms::getCycle(){
+uint16_t JbdBms::getCycle()
+{
   return m_cycle;
 }
-
 
 /**
  * \External Temp Probe
  */
-float JbdBms::getTemp1() {
+float JbdBms::getTemp1()
+{
   return m_Temp1;
 }
 
 /**
  * \Onboard Temp
  */
-float JbdBms::getTemp2() {
+float JbdBms::getTemp2()
+{
   return m_Temp2;
 }
 
-packCellInfoStruct JbdBms::getPackCellInfo() {
+packCellInfoStruct JbdBms::getPackCellInfo()
+{
   return m_packCellInfo;
 }
 
-void JbdBms::sendReqBasicMessage() {
-  uint8_t reqMessage[] = { 0xDD, 0xA5, 0x03, 0x00, 0xFF, 0xFD, 0x77 };
+void JbdBms::sendReqBasicMessage()
+{
+  uint8_t reqMessage[] = {0xDD, 0xA5, 0x03, 0x00, 0xFF, 0xFD, 0x77};
   m_port->write(reqMessage, 7);
 }
 
-void JbdBms::sendCellMessage() {
-  uint8_t reqMessage[] = { 0xDD, 0xA5, 0x04, 0x00, 0xFF, 0xFC, 0x77 };
+void JbdBms::sendCellMessage()
+{
+  uint8_t reqMessage[] = {0xDD, 0xA5, 0x04, 0x00, 0xFF, 0xFC, 0x77};
   m_port->write(reqMessage, 7);
 }
 
-void JbdBms::parseReqBasicMessage(uint8_t * t_message) {
-  m_voltage = (float)two_ints_into16(t_message[4], t_message[5])/100;
+void JbdBms::parseReqBasicMessage(uint8_t *t_message)
+{
+  m_voltage = (float)two_ints_into16(t_message[4], t_message[5]) / 100;
   m_current = ((float)two_ints_into16(t_message[6], t_message[7])) * 10;
   m_chargePercentage = t_message[23];
   m_protectionState = ((float)two_ints_into16(t_message[20], t_message[21]));
   m_cycle = ((float)two_ints_into16(t_message[12], t_message[13]));
   m_Temp1 = (((float)two_ints_into16(t_message[27], t_message[28])) - 2731) / 10.00f;
   m_Temp2 = (((float)two_ints_into16(t_message[29], t_message[30])) - 2731) / 10.00f;
-  
 }
 
-void JbdBms::parseReqPackMessage(uint8_t * t_message){ //packCellInfoStruct * t_packCellInfo) {
+void JbdBms::parseReqPackMessage(uint8_t *t_message)
+{ //packCellInfoStruct * t_packCellInfo) {
   uint16_t _cellSum = 0;
   uint16_t _CellLow = 5000; //5v
   uint16_t _CellHigh = 0;
 
-  m_packCellInfo.NumOfCells = t_message[3] / 2;  //Data length * 2 is number of cells !!!!!!
+  m_packCellInfo.NumOfCells = t_message[3] / 2; //Data length * 2 is number of cells !!!!!!
 
   byte offset = 4;
   for (byte i = 0; i < m_packCellInfo.NumOfCells; i++)
@@ -140,18 +157,17 @@ void JbdBms::parseReqPackMessage(uint8_t * t_message){ //packCellInfoStruct * t_
 
     if (m_packCellInfo.CellVoltage[i] > _CellHigh)
     {
-        _CellHigh = m_packCellInfo.CellVoltage[i];
+      _CellHigh = m_packCellInfo.CellVoltage[i];
     }
     if (m_packCellInfo.CellVoltage[i] < _CellLow)
     {
-        _CellLow = m_packCellInfo.CellVoltage[i];
+      _CellLow = m_packCellInfo.CellVoltage[i];
     }
 
     m_packCellInfo.CellLow = _CellLow;
     m_packCellInfo.CellHigh = _CellHigh;
     m_packCellInfo.CellDiff = _CellHigh - _CellLow; // Resolution 10 mV -> convert to volts
     m_packCellInfo.CellAvg = _cellSum / m_packCellInfo.NumOfCells;
-
   }
 }
 
@@ -166,8 +182,9 @@ bool JbdBms::readResponce(uint8_t *t_outMessage)
     {
       return false;
     }
-    if (m_port->available() > 0) {
-    // if (Serial2.available() > 0) {
+    if (m_port->available() > 0)
+    {
+      // if (Serial2.available() > 0) {
       uint8_t thisByte = m_port->read();
       // uint8_t thisByte = Serial2.read();
       if (thisByte == 0xDD)
@@ -198,7 +215,6 @@ bool JbdBms::checkCheckSumRecieve(uint8_t *t_message)
   uint16_t checkSumAccepted;
   uint8_t lengthData;
   uint8_t startIndexCS;
-
 
   if (t_message[2] != 0) // Status OK
     return false;
@@ -234,7 +250,8 @@ uint16_t JbdBms::computeCrc16JbdChina(uint8_t *puchMsg, uint8_t usDataLen)
   return checkSum;
 }
 
-uint32_t JbdBms::getMaxTimeout(){
+uint32_t JbdBms::getMaxTimeout()
+{
   return 100;
 }
 
@@ -244,7 +261,7 @@ uint32_t JbdBms::getMaxTimeout(){
 uint16_t JbdBms::two_ints_into16(int highbyte, int lowbyte) // turns two bytes into a single long integer
 {
   uint16_t a16bitvar = (highbyte);
-  a16bitvar <<= 8; //Left shift 8 bits,
+  a16bitvar <<= 8;                   //Left shift 8 bits,
   a16bitvar = (a16bitvar | lowbyte); //OR operation, merge the two
   return a16bitvar;
 }
--- JbdBms.h
+++ JbdBms.h
@@ -15,35 +15,34 @@
 /* \defgroup BMS_STATUS BMS protection status masks
 *  @{
 */
-#define BMS_STATUS_OK       0
-#define BMS_STATUS_CELL_OVP     1
-#define BMS_STATUS_CELL_UVP     2   ///< Power off
-#define BMS_STATUS_PACK_OVP     4
-#define BMS_STATUS_PACK_UVP     8   ///< Power off
-#define BMS_STATUS_CHG_OTP      16
-#define BMS_STATUS_CHG_UTP      32
-#define BMS_STATUS_DSG_OTP      64    ///< Power off
-#define BMS_STATUS_DSG_UTP      128   ///< Power off
-#define BMS_STATUS_CHG_OCP      256
-#define BMS_STATUS_DSG_OCP      512   ///< Power off
-#define BMS_STATUS_SHORT_CIRCOUT  1024  ///< Power off
-#define BMS_STATUS_AFE_ERROR    2048
-#define BMS_STATUS_SOFT_LOCK    4096
-#define BMS_STATUS_CHGOVERTIME    8192
-#define BMS_STATUS_DSGOVERTIME    16384 ///< Power off
+#define BMS_STATUS_OK 0
+#define BMS_STATUS_CELL_OVP 1
+#define BMS_STATUS_CELL_UVP 2 ///< Power off
+#define BMS_STATUS_PACK_OVP 4
+#define BMS_STATUS_PACK_UVP 8 ///< Power off
+#define BMS_STATUS_CHG_OTP 16
+#define BMS_STATUS_CHG_UTP 32
+#define BMS_STATUS_DSG_OTP 64  ///< Power off
+#define BMS_STATUS_DSG_UTP 128 ///< Power off
+#define BMS_STATUS_CHG_OCP 256
+#define BMS_STATUS_DSG_OCP 512        ///< Power off
+#define BMS_STATUS_SHORT_CIRCOUT 1024 ///< Power off
+#define BMS_STATUS_AFE_ERROR 2048
+#define BMS_STATUS_SOFT_LOCK 4096
+#define BMS_STATUS_CHGOVERTIME 8192
+#define BMS_STATUS_DSGOVERTIME 16384 ///< Power off
 /* @} Power off errors 100011011001010 - 0x46CA*/
-#define BMS_POWER_OFF_ERRORS    0x46CA
+#define BMS_POWER_OFF_ERRORS 0x46CA
 
-
-typedef struct packCellInfoStruct{
+typedef struct packCellInfoStruct
+{
   uint8_t NumOfCells;
   uint16_t CellVoltage[15]; // Max 16 Cell BMS
-  uint16_t CellLow; // Minimal voltage in cells
-  uint16_t CellHigh; // Maximal voltage in cells
-  uint16_t CellDiff; // difference between highest and lowest
-  uint16_t CellAvg; // Average voltage in cells
+  uint16_t CellLow;         // Minimal voltage in cells
+  uint16_t CellHigh;        // Maximal voltage in cells
+  uint16_t CellDiff;        // difference between highest and lowest
+  uint16_t CellAvg;         // Average voltage in cells
 } packCellInfoStruct;
-
 
 /**
  * \brief Class for working with BMS, which are compatible with the program
@@ -58,13 +57,12 @@ typedef struct packCellInfoStruct{
 class JbdBms
 {
 public:
-  JbdBms(HardwareSerial * t_hardwareSerial);
-  JbdBms(SoftwareSerial * t_softwareSerial);
+  JbdBms(HardwareSerial *t_hardwareSerial);
+  JbdBms(SoftwareSerial *t_softwareSerial);
   JbdBms(int rx, int tx);
 
   bool readBmsData();
   bool readPackData();
-
 
   float getVoltage();
   float getCurrent();
@@ -76,7 +74,7 @@ public:
   packCellInfoStruct getPackCellInfo();
 
 private:
-  Stream * m_port;
+  Stream *m_port;
   bool m_hwserial;
   float m_voltage = 0;
   float m_current = 0;
@@ -89,15 +87,15 @@ private:
   packCellInfoStruct m_packCellInfo = {0};
 
   void sendReqBasicMessage();
-  void parseReqBasicMessage(uint8_t * t_message);
+  void parseReqBasicMessage(uint8_t *t_message);
 
   void sendCellMessage();
-  void parseReqPackMessage(uint8_t * t_message);
+  void parseReqPackMessage(uint8_t *t_message);
 
-  bool readResponce(uint8_t * t_outMessage);
+  bool readResponce(uint8_t *t_outMessage);
 
-  bool checkCheckSumRecieve(uint8_t * t_message);
-  uint16_t computeCrc16JbdChina(uint8_t * puchMsg, uint8_t usDataLen);
+  bool checkCheckSumRecieve(uint8_t *t_message);
+  uint16_t computeCrc16JbdChina(uint8_t *puchMsg, uint8_t usDataLen);
   float converUint32ToFloat(uint32_t number);
   uint16_t two_ints_into16(int highbyte, int lowbyte);
 
